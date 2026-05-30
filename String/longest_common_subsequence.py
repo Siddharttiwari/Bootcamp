a8ab4d3dd2ad7607eaa6d6441a696cbc3340a879
Siddharttiwari <siddharthtiwari952@gmail.com>
2026-05-30 15:00:54 +0530

    updated

---
+++ String/longest_common_subsequence.py
@@ -0,0 +1,17 @@
+"""
+THIS WILLL ONLY SHOW LENGTH OF THE SUBSEQUENCE
+def longest_common_subsequence(str1,str2,m,n):
+    if n==0 or m==0:
+        return 0
+    
+    if str1[m-1]==str2[n-1]:
+        return 1+longest_common_subsequence(str1,str2,m-1,n-1)
+    else:
+        return max(longest_common_subsequence(str1,str2,m,n-1),longest_common_subsequence(str1,str2,m-1,n))
+
+str1=input()
+str2=input()
+m=len(str1)
+n=len(str2)
+print(longest_common_subsequence(str1,str2,m,n))
+"""
