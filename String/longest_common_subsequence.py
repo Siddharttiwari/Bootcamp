55e06fb81661fa29e198d41fa63b216eeb1e2465
Siddharttiwari <siddharthtiwari952@gmail.com>
2026-05-30 15:33:47 +0530

    updated

--- String/longest_common_subsequence.py
+++ String/longest_common_subsequence.py
@@ -15,3 +15,27 @@ m=len(str1)
 n=len(str2)
 print(longest_common_subsequence(str1,str2,m,n))
 """
+"""
+THIS WILL PRINT THE SUBSEQUECE
+def longest_common_subsequence(str1,str2,m,n):
+    if n==0 or m==0:
+        return ""
+    
+    if str1[m-1]==str2[n-1]:
+        return longest_common_subsequence(str1,str2,m-1,n-1) + str1[m-1]
+    else:
+        lcs1=longest_common_subsequence(str1,str2,m,n-1)
+        lcs2=longest_common_subsequence(str1,str2,m-1,n)
+
+        if lcs1>lcs2:
+            return lcs1
+        else:
+            return lcs2
+    
+
+str1=input()
+str2=input()
+m=len(str1)
+n=len(str2)
+print(longest_common_subsequence(str1,str2,m,n))
+"""
