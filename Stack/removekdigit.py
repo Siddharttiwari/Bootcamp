24770d3a42f79b8e00aa94e6418f3a238ea372da
Siddharttiwari <siddharthtiwari952@gmail.com>
2026-06-08 22:58:06 +0530

    updated

---
+++ Stack/removekdigit.py
@@ -0,0 +1,36 @@
+
+def removekdigit(nums,k):
+    st=[]
+    for digit in nums:
+        while st and k > 0 and st[-1] > digit:
+            st.pop() 
+            k -= 1
+
+        st.append(digit)
+
+        while st and k > 0:
+            st.pop() 
+            k -= 1
+        
+        
+        if not st:
+            return "0"
+        
+        
+        res = ""
+        
+        
+        while st:
+            res += st.pop()
+        
+        
+        res = res.rstrip('0')
+        
+        
+        res = res[::-1]
+        
+        
+        if not res:
+            return "0"
+        
+        return res
