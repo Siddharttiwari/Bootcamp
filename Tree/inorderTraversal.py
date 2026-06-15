0090e95561573e2492284c2dab7dd3710b3ad510
Siddharttiwari <siddharthtiwari952@gmail.com>
2026-06-15 23:10:15 +0530

    updated

---
+++ Tree/inorderTraversal.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None
+        self.right=None
+        
+def Inorder(root):
+    if root is None:
+        return 0
+    Inorder(root.left)
+    print(root.data)
+    Inorder(root.right)
+
+
+def treeinput():
+    rootData=int(input())
+    if rootData ==-1:
+        return None
+    root=Node(rootData)
+    leftTree=treeinput()
+    rightTree=treeinput()
+    root.left=leftTree
+    root.right=rightTree
+    return root
+
+def printTree(root):
+    if root ==None:
+        return
+    print(root.data,end=":")
+    if root.left!=None:
+        print("L",root.left.data,end=",")
+    if root.right!=None:
+        print("R",root.right.data,end=",")
+    print()
+    printTree(root.left)
+    printTree(root.right)
+root=treeinput()
+printTree(root)
+print("Inorder Traversal:")
+Inorder(root)
